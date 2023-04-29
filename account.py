7f0af083923e814eaeaae65d39c6ea7926b621d7
Gerald Pruitt <geraldpruitt64@gmail.com>
2023-04-28 20:49:30 -0500

    Update account.py

--- account.py
+++ account.py
@@ -1,84 +1,62 @@
 class Account:
     
-    """
-    This class allows you to keep track of the amount
-    of money in your account.
-    
-    :param __account_name: the users name
-    :param __account_name: the users account balance
-    """
-    
-    def __init__(self, name):
+
+    def __init__(self, name: str):
         """
-        Parameters
-        ----------
-        name : name of user
-
-        Returns
-        -------
-        None.
-
+        Constructor to create initialstate of a account
+        :param account_name: Person's first name
+        :param account_balance: amount of money in the account
         """
         self.__account_name = name
         self.__account_balance = 0
     
-    def deposit(self, amount) -> bool:
+    def deposit(self, amount: int) -> bool:
         """
         add given ammount of money to your balance
-
         Parameters
         ----------
         amount : amount of money to be added
-
         Returns : boolean
         -------
-
         """
+
         if amount > 0:
-            self.__account_balance += amount # 8
+            self.__account_balance += amount
             return True
         return False
     
-    def withdraw(self, amount) -> bool:
+
+    def withdraw(self, amount: int) -> bool:
         """
         removes a given amount of money from the
         user's balance
-
         Parameters
         ----------
         amount : amount of money to be removed
-
         Returns: boolean
         -------
         bool
             chacks if the amount removed is greater than 0
             and you are removing less than the current balance
-
         """
+
         if 0 < amount <= self.__account_balance:
-            self.__account_balance -= amount # 4
+            self.__account_balance -= amount
             return True
         return False
     
+
     def get_balance(self) -> int:
         """
-        
-
-        Returns
-        -------
-        user's current balance
-
+        Method to get the person's current balance
+        :return: current balance
         """
         return self.__account_balance
     
     def get_name(self) -> int:
         """
-        
-
-        Returns
-        -------
-        users current name
-
+        Method to get the user's name
+        :return: Person's name
         """
-        return self.__account_name
 
+        return self.__account_name
